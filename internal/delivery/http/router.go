e9b093d0ad5afbe812e3b4e2f9a2850a5fe3a023
Dante Dev <jdap.dmc@gmail.com>
2026-07-20 16:58:30 -0400

    feat: community service

--- internal/delivery/http/router.go
+++ internal/delivery/http/router.go
@@ -9,7 +9,7 @@ func SetupRoutes(app *echo.Echo, h *CommunityHandler, jwtSecret string) {
 	v1 := app.Group("/api/v1")
 
 	jwtMd := middleware.JWTMiddleware(jwtSecret)
-	adminOrProf := middleware.RequireRole("ADMIN", "PROFESSOR")
+	adminOrProf := middleware.RequireRole("ADMIN", "PROFESSOR", "STUDENT")
 
 	v1.GET("/communities", h.List, jwtMd)
 	v1.POST("/communities", h.Create, jwtMd, adminOrProf)
